6202e2d6b6a16d890fcd469e08009476d2af9bc7
yousuf <syed.yousuf@navana.com>
2022-02-28 14:29:22 +0600

    DB credentials updated for testing on navcom.agency

--- includes/config.php
+++ includes/config.php
@@ -2,8 +2,8 @@
 
 // Database Constants
 define("DB_HOST", "localhost");
-define("DB_USER", "navana_admin");
+define("DB_USER", "navakubf_test");
 define("DB_PASSWORD", "Navana_Group");
-define("DB_NAME", "navana_group");
+define("DB_NAME", "navakubf_navana");
 
 ?>
